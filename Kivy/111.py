4ea9be98a95197332d63115da4cbdadce85aed2f
centurionservise <centurionservise@rambler.ru>
2018-09-27 00:50:26 +0300

    pystom

--- Kivy/111.py
+++ Kivy/111.py
@@ -14,37 +14,61 @@ Config.set('graphics','height',500)
 
 class sergApp(App):
 
+    formula='0'
+
     def add_number(self, instance):
-        self.formula+=str(instance.text)
+        if instance.text==',' and self.formula.find(instance.text)==-1:
+            self.formula+=instance.text
+            self.lbl.text=self.formula
+        elif instance.text==',' and self.formula.find(instance.text)!=-1:
+            pass
+        elif self.formula=='0' and instance.text.isdigit()==True:
+            print('***** IsDigit *****')
+            temp_oper=int(self.formula)+int(instance.text)
+            self.lbl.text=self.formula=str(temp_oper)
+        else:
+            self.formula+=instance.text
+            self.lbl.text=self.formula
+    
+    def add_operator (self, instance):
+        pass
+    
+    def get_result (self, instance):
+        pass
+
+    def sett_zerro (self, instance):
+        self.formula='0'
+        self.lbl.text='0'
+
 
     def build(self):
-        self.formula='0'
+        # self.formula='0'
         # scat=Scatter()
         gl=GridLayout(cols=4, spacing=5, size_hint=(1,.6))
         bl=BoxLayout(orientation='vertical', padding=25)
         # scat.add_widget(fl)
-        self.lbl=Label(text='012345678', font_size=40, size_hint=(1,.4), halign='right', valign='center', text_size=(400-50, 500*.4-50))
+        self.lbl=Label(text='0', font_size=40, size_hint=(1,.4), halign='right', valign='center', text_size=(400-50, 500*.4-50))
         bl.add_widget(self.lbl)
 
         gl.add_widget(Button(text='7', on_press=self.add_number))
         gl.add_widget(Button(text='8', on_press=self.add_number))
         gl.add_widget(Button(text='9', on_press=self.add_number))
-        gl.add_widget(Button(text='X', on_press=self.add_number))
+        gl.add_widget(Button(text='x', on_press=self.add_operator))
 
         gl.add_widget(Button(text='4', on_press=self.add_number))
         gl.add_widget(Button(text='5', on_press=self.add_number))
         gl.add_widget(Button(text='6', on_press=self.add_number))
-        gl.add_widget(Button(text='-', on_press=self.add_number))
+        gl.add_widget(Button(text='-', on_press=self.add_operator))
 
         gl.add_widget(Button(text='1', on_press=self.add_number))
         gl.add_widget(Button(text='2', on_press=self.add_number))
         gl.add_widget(Button(text='3', on_press=self.add_number))
-        gl.add_widget(Button(text='+', on_press=self.add_number))
+        gl.add_widget(Button(text='+', on_press=self.add_operator))
 
-        gl.add_widget(Widget())
+        gl.add_widget(Button(text='C', on_press=self.sett_zerro))
         gl.add_widget(Button(text='0', on_press=self.add_number))
         gl.add_widget(Button(text=',', on_press=self.add_number))
-        gl.add_widget(Button(text='=', on_press=self.add_number))
+        gl.add_widget(Button(text='=', on_press=self.get_result))
 
         bl.add_widget(gl)
 
